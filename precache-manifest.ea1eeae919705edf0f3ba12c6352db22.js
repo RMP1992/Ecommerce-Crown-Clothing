0f6987b75e24163b507dea70a73f1e4a2c018958
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-04-07 21:53:35 +0100

    Updates

---
+++ precache-manifest.ea1eeae919705edf0f3ba12c6352db22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4373ee2a32850d9c1d03e24a57a4c1bf",
+    "url": "/Ecommerce-Crown-Clothing/index.html"
+  },
+  {
+    "revision": "2146f055a33fd8774c14",
+    "url": "/Ecommerce-Crown-Clothing/static/css/main.b68ea26a.chunk.css"
+  },
+  {
+    "revision": "3bab26ba8750483e1d28",
+    "url": "/Ecommerce-Crown-Clothing/static/js/2.7e275c00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ecommerce-Crown-Clothing/static/js/2.7e275c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2146f055a33fd8774c14",
+    "url": "/Ecommerce-Crown-Clothing/static/js/main.5f310031.chunk.js"
+  },
+  {
+    "revision": "346a2d2f1f17b713cf7b",
+    "url": "/Ecommerce-Crown-Clothing/static/js/runtime-main.6a5f88ea.js"
+  }
+]);
