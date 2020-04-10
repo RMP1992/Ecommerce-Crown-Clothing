b59497718917ef145ec1786e47857a8110c129ae
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-04-10 22:21:37 +0100

    added header component and links to home, shop page and contact me, also styled the header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './component/pages/homepage/homepage.component';
 import ShopPage from './component/pages/shop/shop.component';
+import Header from './component/header/header.component';
 import './App.css';
 
 
@@ -9,6 +10,8 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
+      {/* I have placed the Header outside of the Switch because switch only displays one component at a time and I want the header to always be displayed */}
       <Switch>
         {/* Switch render only one page the moment one path matches, it avoids accidently rendering multiple components */}
         {/* exact makes it so that the path in this case'/' has to match in order for the component to be rendered */}
