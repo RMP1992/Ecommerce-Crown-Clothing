4bc84da84652dec0f0653fbc173a164776d40b96
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-04-10 18:21:05 +0100

    added routes and switch to the hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './component/pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        {/* Switch render only one page the moment one path matches, it avoids accidently rendering multiple components */}
+        {/* exact makes it so that the path in this case'/' has to match in order for the component to be rendered */}
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+      
     </div>
   );
 }
