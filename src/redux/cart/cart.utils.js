ae329f0fad5021f5ccc0ff9804e585920833dd4d
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-05-09 14:16:02 +0100

    created cart item component and connected it with cart items reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,13 +1,15 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    //cartItems is our current state (array) and cartItemToAdd is the new item
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
-    //this function comparees the id of existing cartItems to the new cartItemToAdd id if it matches it will set it the existingCartItem otherwise it will be undefined
-    if(existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-            ? {...cartItem, quantity: cartItem.quantity + 1}
-            : cartItem
-            )
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
     }
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
-}
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
