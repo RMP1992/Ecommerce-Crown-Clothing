6687c2f040bb06cf3f2bb84db2e8bd291183baa0
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-05-15 20:51:01 +0100

    updated how the applications targets the url params

--- src/component/pages/shop/shop.component.jsx
+++ src/component/pages/shop/shop.component.jsx
@@ -1,21 +1,15 @@
+  
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-
-import CollectionsOverview  from '../../collections-overview/collections-overview.component';
+import CollectionsOverview from '../../collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-
-const ShopPage = (match) => {
-    console.log(match)
-    return(
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        </div>
-    )
-}
-
-
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
